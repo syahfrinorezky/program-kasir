4f44c9a86d1e80bf00b9df8fed4d52efc28b5bf9
syahfrinorezky <syahfrinorezky@gmail.com>
2025-09-24 00:05:23 +0800

    feat: add temporary auth controller, with register logic

---
+++ app/Controllers/Auth.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\UserModel;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class Auth extends BaseController
+{
+    public function register()
+    {
+        helper('form');
+        return view('auth/register');
+    }
+
+    public function registerSave()
+    {
+        $model  = new UserModel();
+
+        $data = [
+            'username'     => $this->request->getPost('username'),
+            'password'     => password_hash($this->request->getPost('password'), PASSWORD_BCRYPT),
+            'nama_lengkap' => $this->request->getPost('nama_lengkap'),
+            'level'        => 'user',
+        ];
+
+        $insertData = $model->insert($data);
+
+        if ($insertData === false) {
+            return redirect()->back()->withInput()->with('errors', $model->errors());
+        }
+
+        return redirect()->to('/login')->with('success', 'Pendaftaran berhasil, silakan login.');
+    }
+
+
+
+    public function login()
+    {
+        helper('form');
+        return view('auth/login');
+    }
+}
